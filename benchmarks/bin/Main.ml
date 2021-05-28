dd50f31cbc69362242df7c1d5924690349261e7d
Eric Campbell <1234goal@gmail.com>
2021-05-28 17:03:34 -0400

    fix typo in instrumentation def

--- benchmarks/bin/Main.ml
+++ benchmarks/bin/Main.ml
@@ -1,6 +1,16 @@
+open Core
 module Bench = Pbench.Bench
 
-let () =
-  Bench.benchmark 2
-  |> Printf.printf "%s\n%!"
+let main : Command.t =
+  let open Command.Let_syntax in
+  Command.basic ~summary:"Invokes the Princess benchmarker"
+    [%map_open
+     let n = anon ("num tables" %: int) in
+         fun () ->         
+         Bench.benchmark n
+         |> Printf.printf "%s\n%!"
+    ]
+
+
+let () = Command.run main
 
